34812b3472e040d2e2693be4852f35475e322e2f
akashJh <69488117+akashJh@users.noreply.github.com>
2020-09-14 02:17:08 +0530

    Add files via upload

--- Stego.py
+++ Stego.py
@@ -1,19 +1,22 @@
 #Image Steganography Project
 #Open an Image and Look at the Pixels in Hex
-#A Delimmeter of 15-1 and a 0 at the end to specify End of the Message helps in Retrieving
+#A Delimmeter of 15-1 and 0 at the end to specify End of the Message helps in Retrieving
+
 from PIL import Image
 import binascii
-import optparse
+
 #Helper Functions
 def rgb2Hex(r,g,b):
 	return '#{:02x}{:02x}{:02x}'.format(r,g,b)
 
 def hex2Rgb(hexcode):
-	return 	tuple(map(ord,hexcode[1:].decode('hex')))
+	hexcode=hexcode.lstrip('#')
+	return tuple(int(hexcode[i:i+1],16) for i in (0,2,4))
 
 def str2Bin(message):
-	binary=bin(int(binascii.hexlify(message.encode()),16))
-	return binary[:2]
+	#binary=bin(int(binascii.hexlify(message.encode()),16))
+	binary= ''.join(format(ord(i), 'b') for i in message) 
+	return binary
 
 def bin2Str(binary):
 	message=binascii.unhexlify('%x' % (int('0b'+binary,2)))
@@ -22,7 +25,9 @@ def bin2Str(binary):
 #Four Operation Functions 
 def encode(hexcode,digit):
 	if hexcode[-1] in ('0','1','2','3','4','5'):
+		print("Previous Hex Code->",hexcode)
 		hexcode=hexcode[:-1]+digit
+		print("After Changing Hexcode->",hexcode)
 		return hexcode
 	else:
 		return None
@@ -39,12 +44,10 @@ def hide(filename,message):
 	binary=str2Bin(message)+'1111111111111110'
 	#Hide the binary into the Image and End it with 15-1 and 0 as delimetter
 	#Check if Image is Editable
-	if img.mode in ('RGBA'):
+	if img.mode in ('PRGBA'):
 		img=img.convert('RGBA')
 		datas=img.getdata()
 		#Changing the whole data into Newdata
-		#print("Image Data:")
-		#print(datas)
 		newData=[]
 		digit=0
 		for item in datas:
@@ -53,12 +56,14 @@ def hide(filename,message):
 				if(newPix==None):
 					newData.append(item)
 				else:
-					newData.append(newPix)
+					#if Hiding is Succesfull insert the new Pixel with Hidden Data
+					r,g,b=hex2Rgb(newPix)
+					newData.append((r,g,b))
 			else:
 				newData.append(item)
-		#print("New Data Made \n",newData)
 		img.putdata(newData)
-		img.save(filename,"PNG")
+		new_name="hidden"
+		img.save(new_name,"PNG")
 		return "Data Hidden"
 	else:
 		print("Image Mode:{}".format(img.mode))
@@ -67,7 +72,7 @@ def hide(filename,message):
 def retrive(filename):
 	img=Image.open(filename)
 	binary=''
-	if img.mode in ('RGBA'):
+	if img.mode in ('PRGBA'):
 		img=img.convert('RGBA')
 		datas=img.getdata()
 
@@ -81,21 +86,20 @@ def retrive(filename):
 				if(binary[:-16]=='1111111111111110'):
 					print("Message Found: ")
 					return bin2Str(binary[:-16])
-		return bin2Str(binary)
+		print("Retrieved Binary is:",binary)
+		return bin2Str(binary)[1:]
 	return "Incorrect Image Format!!"
 
 def main():
-	parser=optparse.OptionParser("Usage %prog"+'-e/-d <target file>')
-	parser.add_option("-e",dest='hide',type='string',help="target Picture Path to hide text")
-	parser.add_option("-d",dest='retrive',type='string',help="target Picture Path to retrive") 
-	(options,args)=parser.parse_args()
-	if( options.hide!=None):
-		text=input("Enter the message to hide->")
-		print(hide(options.hide,text))
-	elif( options.retrive!=None):
-		print(retrive(options.retrive))
+	pic=input("Enter the Image:")
+	print("1.Encode\n2.Decode")
+	opt=int(input("Choose [1/2] : "))
+	if(opt==1):
+		text=input("Enter the message to hide: ")
+		print(hide(pic,text))
+	elif(opt==2):
+		print(retrive(pic))
 	else:
-		print(parser.usage)
 		exit(0)
 
 main()
